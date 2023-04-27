95ab6a403f3e963583c061c21c33309419f4271e
Shreshtha Sharma <99015175+shreshtha-sharma@users.noreply.github.com>
2023-04-27 15:14:09 +0530

    cors and inventory edit fix

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userApi = require('./User/User.api');
 const Product = require('./products/products');
+const cors = require('cors'); // Import the cors package
+
+app.use(cors()); // Enable CORS for all routes run 
+
+const router = express.Router();
+
+
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.6:8080');
@@ -35,12 +43,54 @@ app.get('/products', async (req, res) => {
   }
 });
 
+// Express application
+app.use("/products", router);
+
+
+
+
 // Define a POST route for products
 app.post('/products', async (req, res) => {
   try {
     let product = new Product(req.body);
     await product.save();
     res.status(201).json({ message: 'Product created successfully', data: product });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Router configuration
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findOne({ p_id: id });
+
+    if (!product) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const payload = req.body;
+
+    // Find the product by ID and update it
+    const updatedProduct = await Product.findOneAndUpdate({ p_id: id }, payload, {
+      new: true, // Return the updated product
+    });
+
+    if (!updatedProduct) {
+      res.status(404).json({ error: "Product not found" });
+    } else {
+      res.status(200).json(updatedProduct);
+    }
   } catch (error) {
     res.status(500).send(error);
   }
